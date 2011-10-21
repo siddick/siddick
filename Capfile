62593058e6d302bf356b71dd7b1dc817c089cdce
Mohammed siddick <siddick@gmail.com>
2011-10-21 19:36:37 +0530

    gem modified

--- Capfile
+++ Capfile
@@ -7,4 +7,5 @@ Dir['vendor/gems/*/recipes/*.rb','vendor/plugins/*/recipes/*.rb'].each { |plugin
 
 load 'config/deploy' # remove this line to skip loading any of the default tasks
 
+require 'bundler/capistrano'
 require 'capistrano/ext/multistage'
--- config/deploy.rb
+++ config/deploy.rb
@@ -2,12 +2,13 @@ set :application, "siddick"
 set :repository,  "git://github.com/siddick/siddick.git"
 set :scm, :git
 
+set :user, "apps"
 set :stages, ["staging", "production"]
 set :default_stage, "staging"
 
 set :use_sudo, false
 set :bundle_exec, "cd #{current_path}; bundle exec"
-set :thin, "#{bundle_exec} thin -C #{shared_path}/thin.conf"
+set :thin_exec, "#{bundle_exec} thin -C #{shared_path}/thin.conf"
 set :rake, "#{bundle_exec} rake"
 
 namespace :deploy do
@@ -15,15 +16,15 @@ namespace :deploy do
     run "#{rake} assets:precompile"
   end
   task :start do
-    run "#{thin} start"
+    run "#{thin_exec} start"
   end
   task :stop do
-    run "#{thin} stop"
+    run "#{thin_exec} stop"
   end
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{thin} restart"
+    run "#{thin_exec} restart"
   end
-  tast :thin do
-    run "#{thin} config -p 3000"
+  task :thin do
+    run "#{thin_exec} config -p 3000"
   end
 end
