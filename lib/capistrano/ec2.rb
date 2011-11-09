fd05354a91c40a4ce9339b4cd23f1be918d8e8be
Mohammed siddick <siddick@gmail.com>
2011-11-09 16:15:23 +0530

    change ec2 deploy

--- lib/capistrano/ec2.rb
+++ lib/capistrano/ec2.rb
@@ -2,15 +2,24 @@ require 'aws/ec2'
 
 Capistrano::Configuration.instance.load do
 
+  # AWS authentication information
   set :aws_key,     lambda{ ENV["AWS_KEY"] || abort("Need AWS_KEY and AWS_SECRET") }
   set :aws_secret,  lambda{ ENV["AWS_SECRET"] || abort("Need AWS_KEY and AWS_SECRET") }
   
-  set :ec2,           lambda{ AWS::EC2.new( :access_key_id => aws_key, :secret_access_key => aws_secret ) }
-  set :ec2_ami,       "ami-bbf539d2"
-  set :ec2_name_key,  "capistrano-app"
-  set :ec2_name,      lambda{ fetch(:application) }
-  set :ec2_role_key,  "capistrano-role"
-  set :ec2_filter,    lambda{ fetch(:ec2).instances.tagged(ec2_name_key).tagged_values(ec2_name) }
+  # EC2 instance
+  set :ec2_ami,           "ami-a7f539ce"
+  set :ec2_instance_type, "t1.micro"
+  set :ec2_key_pair,      lambda{ ec2_object.key_pairs.first.name }
+  
+  # EC2 tags
+  set :ec2_name_key,      "capistrano-app"
+  set :ec2_name,          lambda{ fetch(:application) }
+  set :ec2_role_key,      "capistrano-role"
+  set :ec2_role,          "web,app,db"
+  
+  # EC2 objects
+  set :ec2_object, lambda{ AWS::EC2.new( :access_key_id => aws_key, :secret_access_key => aws_secret ) }
+  set :ec2_filter, lambda{ ec2_object.instances.tagged(ec2_name_key).tagged_values(ec2_name) }
   
   namespace :ec2 do
     desc "Find the EC2 instances"
@@ -23,11 +32,12 @@ Capistrano::Configuration.instance.load do
     end
     desc "Create EC2 instances"
     task :create do
-      roles = (ENV["ROLE"] || "web,app,db").split(",").uniq
-      instance = fetch(:ec2).instances.create( :image_id => ec2_ami )
-      instance[ec2_name_key] = ec2_name
-      instance[ec2_role_key] = roles.join(",")
-      server( instance.ip_address, *roles )
+      roles = (ENV["ROLE"] || ec2_role).split(",").uniq
+      instance = ec2_object.instances.create( :image_id => ec2_ami,
+        :instance_type => ec2_instance_type, :key_pair => ec2_key_pair )
+      instance.tags[ec2_name_key] = ec2_name
+      instance.tags[ec2_role_key] = roles.join(",")
+      server( instance.ip_address, :new_machine, *roles )
     end
     task :destroy do
       ec2_filter.each{|instance|
@@ -41,6 +51,6 @@ Capistrano::Configuration.instance.load do
     end
   end
   
-  after "multistage:ensure", "ec2:find"
+  before "deploy", "ec2:find"
   
 end
--- lib/capistrano/ubuntu.rb
+++ lib/capistrano/ubuntu.rb
@@ -1,7 +1,12 @@
 Capistrano::Configuration.instance.load do
   
-  set :default_gems,  "bundler foreman"
-  set :postgresql_packages, "postgresql"
+  set :default_rubygems,        "bundler foreman"
+  set :default_ubuntu_packages, [ "language-pack-en-base", 
+    "build-essential libreadline6-dev libssl-dev sqlite3 libsqlite3-dev libxml2-dev git git-core " +
+    "memcached libmemcached-dev libcurl4-openssl-dev imagemagick nodejs s3cmd libxslt1-dev " + 
+    "libffi-dev libyaml-dev libpq-dev" ]
+  set :default_db_packages,     "postgresql"
+  
   namespace :ubuntu do
     task :postgresql, :roles => :db do
       run "sudo apt-get install #{postgresql_packages}"
