f73de80e9ff3c8e2c6b45b504d7cae26fb64f49f
siddick <siddick@gmail.com>
2012-01-18 23:08:30 +0530

    bundle update

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,13 +2,9 @@ require 'spec_helper'
 
 describe PostsController do
 
-  def valid_attributes
-    Factory.attributes_for(:post)
-  end
-
   describe "GET index" do
     it "assigns all posts as @posts" do
-      post = Post.create! valid_attributes
+      post = FactoryGirl.create(:post)
       get :index
       response.should be_success
     end
@@ -16,7 +12,7 @@ describe PostsController do
 
   describe "GET show" do
     it "assigns the requested post as @post" do
-      post = Post.create! valid_attributes
+      post = FactoryGirl.create(:post)
       get :show, :id => post.id
       response.should be_success
     end
