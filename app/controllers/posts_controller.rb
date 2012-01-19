309ea0d8ceae36e4ae0b8ed4cd9745ec9d9e9798
Hello <hello@siddick.in>
2012-01-19 21:56:44 +0530

    Pagination.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,8 @@ class PostsController < InheritedResources::Base
       fulltext params[:q] if params[:q].present?
       with(:published, true)
       with(:published_at).less_than(Time.zone.now)
+      order_by :published_at, :desc
+      paginate :page => params[:page], :per_page => 10
     end
     @posts = @search.results
   end
