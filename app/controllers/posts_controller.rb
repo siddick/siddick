f6cc3738c4e5bf26a92f0a85b47ee7cea06dbf80
siddick <siddick@gmail.com>
2012-01-19 22:39:39 +0530

    remove inherited resource for posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,4 @@
-class PostsController < InheritedResources::Base
+class PostsController < ApplicationController
   def index
     @search = Post.solr_search do
       fulltext params[:q] if params[:q].present?
@@ -8,5 +8,9 @@ class PostsController < InheritedResources::Base
       paginate :page => params[:page], :per_page => 10
     end
     @posts = @search.results
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 end
