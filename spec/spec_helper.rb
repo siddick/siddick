f1956470c8b19e37a65466ddf5a42f170707cc26
siddick <siddick@gmail.com>
2012-01-29 12:05:05 +0530

    devise helper for spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,7 @@ Spork.prefork do
     config.fixture_path = "#{::Rails.root}/spec/fixtures"
     config.use_transactional_fixtures = true
     config.infer_base_class_for_anonymous_controllers = false
+    config.include Devise::TestHelpers, :type => :controller
   end
 end
 
