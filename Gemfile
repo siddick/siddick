590ea2da1398ec806628b2b511d77d30991f27b1
siddick <siddick@gmail.com>
2012-03-07 22:42:17 +0530

    Update rails & simple form version

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.2.1'
+gem 'rails', '3.2.2'
 gem 'devise'
 gem 'activeadmin',  '>= 0.4.0'
 gem 'sass-rails',   '~> 3.2.0'
--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -1,14 +1,21 @@
 # Use this setup block to configure all options available in SimpleForm.
 SimpleForm.setup do |config|
-  config.components     = [ :placeholder, :label, :custom_format ]
-  config.wrapper_class       = "clearfix"
-  config.wrapper_error_class = "error"
-  config.hint_class     = "help-block"
-  config.error_class    = "help-inline"
-end
+  config.wrappers :tag => :div, :class => :clearfix,
+                  :error_class => :error do |b|
+    # Extensions
+    b.use :html5
+    b.optional :pattern
+    b.use :maxlength
+    b.use :placeholder
+    b.use :readonly
 
-SimpleForm::Inputs::Base.class_eval do
-  def custom_format
-    "<div class='input'>#{input}#{error}#{hint}</div>"
+    # Inputs
+    b.use :label
+    b.wrapper :tag => :div, :class => "input" do |ba|
+      ba.use :input
+      ba.use :error, :wrap_with => { :tag => :span, :class => "help-inline" }
+      ba.use :hint,  :wrap_with => { :tag => :span, :class => "help-block" }
+    end
   end
 end
+
