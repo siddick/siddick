83e235c026009778acaecdf0465422363cb7e27f
siddick <siddick@gmail.com>
2012-05-10 12:41:34 +0530

    added extension tables

--- app/workers/generate_content.rb
+++ app/workers/generate_content.rb
@@ -11,7 +11,7 @@ class GenerateContent
     def markdown
       @markdown ||= begin
         html_render = CustomHTMLRender.new( :filter_html => true, :hard_wrap => true )
-        Redcarpet::Markdown.new(html_render, :autolink => true, :no_intra_emphasis => true, :fenced_code_blocks => true)
+        Redcarpet::Markdown.new(html_render, :autolink => true, :no_intra_emphasis => true, :fenced_code_blocks => true, :tables => true)
       end
     end
 
