25c72cb6a459fb254179117da3456852a6219ec2
siddick <siddick@gmail.com>
2012-05-15 15:30:50 +0530

    tag search

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def index
     @posts  = Post.last_posted.page(params[:page])
+    @posts  = @posts.tagged_with(params[:tag]) if params[:tag].present?
   end
 
   def show
