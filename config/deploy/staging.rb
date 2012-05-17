962a8a555df430dac3c898bf3e2a94ede49b89fb
siddick <siddick@gmail.com>
2012-05-17 15:54:36 +0530

    Change the EC2 instance tag names

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,3 +2,5 @@ set :application, "siddick_staging"
 set :rails_env,   "staging"
 set :deploy_to, "/u/apps/siddick_staging"
 set :deploy_port, "5000"
+
+set :ec2_name, "siddick.in"
--- lib/capistrano/ec2.rb
+++ lib/capistrano/ec2.rb
@@ -5,22 +5,22 @@ Capistrano::Configuration.instance.load do
   # AWS authentication information
   set :aws_key,     lambda{ ENV["AWS_KEY"] || abort("Need AWS_KEY and AWS_SECRET") }
   set :aws_secret,  lambda{ ENV["AWS_SECRET"] || abort("Need AWS_KEY and AWS_SECRET") }
-  
+
   # EC2 instance
   set :ec2_ami,           "ami-a7f539ce"
   set :ec2_instance_type, "t1.micro"
   set :ec2_key_pair,      lambda{ ec2_object.key_pairs.first.name }
-  
+
   # EC2 tags
-  set :ec2_name_key,      "capistrano-app"
+  set :ec2_name_key,      "Name"
   set :ec2_name,          lambda{ fetch(:application) }
-  set :ec2_role_key,      "capistrano-role"
+  set :ec2_role_key,      "Role"
   set :ec2_role,          "web,app,db"
-  
+
   # EC2 objects
   set :ec2_object, lambda{ AWS::EC2.new( :access_key_id => aws_key, :secret_access_key => aws_secret ) }
   set :ec2_filter, lambda{ ec2_object.instances.tagged(ec2_name_key).tagged_values(ec2_name) }
-  
+
   namespace :ec2 do
 
     desc "Find the EC2 instances"
@@ -29,7 +29,7 @@ Capistrano::Configuration.instance.load do
         puts("Application: #{application}")
         ec2_filter.each{|instance|
           puts("Server: #{instance.ip_address} = #{instance.tags[ec2_role_key]}")
-          roles = instance.tags[ec2_role_key].split(",")
+          roles = ( instance.tags[ec2_role_key] || ec2_role ).split(",")
           server( instance.ip_address, *roles, :primary => true )
         }
         @ec2_find_already_executed = true
@@ -58,8 +58,8 @@ Capistrano::Configuration.instance.load do
       }
     end
   end
-  
+
   #before "deploy", "ec2:find"
   after "multistage:ensure", "ec2:find"
-  
+
 end
