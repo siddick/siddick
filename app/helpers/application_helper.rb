60edbbb00242d6c03ec8fba1cffedfcb603f9581
siddick <siddick@gmail.com>
2012-05-25 17:46:17 +0530

    correct html tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
   def title
-    @title ||= @post && @post.title
+    @title ||= @post.try(:title) || params[:action].humanize
   end
+
+  def title=(page_title)
+    @title = page_title
+  end
+
 end
