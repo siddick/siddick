2391959e34a8068e2492e9dd09981c717f37e530
siddick <siddick@gmail.com>
2012-06-15 12:06:02 +0530

    includes tags list on index page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts  = Post.published.page(params[:page])
+    @posts  = Post.published.includes(:tags).page(params[:page])
     @posts  = @posts.tagged_with(params[:tag]) if params[:tag].present?
     @title  = params[:tag].try(:humanize) || "Home"
   end
