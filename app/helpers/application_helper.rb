35b7045ef71d5385e58b3693e3974380a5cc3d24
siddick <siddick@gmail.com>
2012-07-04 23:26:56 +0530

    added page header images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,15 @@ module ApplicationHelper
     @title = page_title
   end
 
+  def body_class
+    "controller-#{params[:controller]} action-#{params[:action]}"
+  end
+
+  def post_page_header_class
+    class_names = []
+    class_names.push("type-#{(@post.type||"post").downcase}") if @post.present? 
+    class_names.push("tag tag-#{params[:tag]}") if params[:tag].present? 
+    class_names.join(" ")
+  end
+
 end
